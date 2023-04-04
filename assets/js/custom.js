ff4b03d279d6a1b215bec98b031fe1e6034b704e
euni0224 <122070570+euni0224@users.noreply.github.com>
2023-04-04 15:11:53 +0900

    프로젝트슬라이드

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,25 @@
 $(function () {
-    history.scrollRestoration = "manual"
+    //history.scrollRestoration = "manual"
     //인트로
     $('.postit-item').click(function(){
         $(this).toggleClass('show');
+        card1 = gsap.timeline({
+            defaults: {
+                duration: 0.2,
+            },
+        })
+        num=$(this).find('.card-area')
+        card1
+        .to(num, {'transform': 'rotate(45deg)'})
     })
+
+// 프로젝트 슬라이드
+    var swiper2 = new Swiper(".pro", {
+        slidesPerView: 1.2,
+        spaceBetween:30,
+        navigation: {
+            nextEl: ".next",
+            prevEl: ".prev",
+            },
+    });
 })
