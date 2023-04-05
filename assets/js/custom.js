07bb04a49e12ca5181ba35be32efeacbcb67427e
euni0224 <122070570+euni0224@users.noreply.github.com>
2023-04-05 14:11:31 +0900

    인트로삽입

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,28 @@
 $(function () {
-    //history.scrollRestoration = "manual"
+    history.scrollRestoration = "manual"
     //인트로
-    $('.postit-item').click(function(){
-        $(this).toggleClass('show');
-        card1 = gsap.timeline({
-            defaults: {
-                duration: 0.2,
-            },
-        })
-        num=$(this).find('.card-area')
-        card1
-        .to(num, {'transform': 'rotate(45deg)'})
+    //gsap.set('body', { overflow: 'hidden'})
+    intro = gsap.timeline({
+        defaults: {
+            duration: 0.6,
+        }
     })
+    intro
+    .addLabel('b')
+    .to('.hd-intro .intro-list .bg1', { yPercent: -10 },'b')
+    .to('.hd-intro .intro-list .bg2', {delay: 0.1, yPercent: -8 },'b')
+    .to('.hd-intro .intro-list .bg3', { delay: 0.2, yPercent: -6 },'b')
+    .to('.hd-intro .intro-list .bg1', { delay: 0.25,yPercent: -100 },'b')
+    .to('.hd-intro .intro-list .bg2', {delay: 0.35, yPercent: -100 },'b')
+    .to('.hd-intro .intro-list .bg3', { delay: 0.45, yPercent: -100 },'b')
+    .to('.hd-intro .intro-list .bg3 span',{yPercent: -110,stagger:0.3,})
+    .addLabel('a')
+    .to('.hd-intro .intro-list .bg3', { yPercent: -200 },'a')
+    .to('.hd-intro .intro-list .bg2', {delay: 0.15, yPercent: -200 },'a')
+    .to('.hd-intro .intro-list .bg1', {delay: 0.25, yPercent: -200 },'a')
+    
 
-// 프로젝트 슬라이드
+    // 프로젝트 슬라이드
     var swiper2 = new Swiper(".pro", {
         slidesPerView: 1.2,
         spaceBetween:30,
